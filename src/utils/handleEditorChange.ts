8340b26cc82bd8724879db7cab1b40d7fd0f2014
kbzcraft <ytkbzcraft@gmail.com>
2024-09-26 16:15:56 +0545

    delete all

--- src/utils/handleEditorChange.ts
+++
@@ -1,8 +0,0 @@
-import useCodeStore from "@/lib/codeStore";
-
-export const handleEditorChange = (value: string | undefined) => {
-  const { setCode } = useCodeStore();
-  if (value !== undefined) {
-    setCode(value);
-  }
-};

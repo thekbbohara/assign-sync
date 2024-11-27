1cedbcc84990c47314dd017e25eca3f0ce67bd7d
thekbbohara <ytkbzcraft@email.com>
2024-11-27 14:35:02 +0545

    class model

---
+++ src/model/assignment.ts
@@ -0,0 +1,27 @@
+import { model, Model, models, type ObjectId, Schema } from "mongoose";
+
+interface IAssignment extends Document {
+  title: string;
+  description?: string;
+  class: ObjectId;
+  dueDate?: Date;
+  starterCode?: string;
+  testCases: string[];
+  solution?: string;
+}
+
+const assignSchema = new Schema<IAssignment>({
+  title: { type: String, required: true },
+  description: { type: String },
+  class: { type: Schema.Types.ObjectId, ref: "Class", required: true },
+  dueDate: { type: Date },
+  starterCode: { type: String },
+  testCases: { type: [String] },
+  solution: { type: String },
+});
+
+const Assignment =
+  (models.Assignment as Model<IAssignment>) ||
+  model<IAssignment>("Assignment", assignSchema);
+
+export default Assignment;

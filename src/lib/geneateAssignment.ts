a497b2f2e3a19d7e370f5f84794247d55415eeb8
thekbbohara <ytkbzcraft@gmail.com>
2024-12-01 07:27:17 +0545

    get test cases unique  id

--- src/lib/geneateAssignment.ts
+++ src/lib/geneateAssignment.ts
@@ -1,10 +1,14 @@
-import { GoogleGenerativeAI, SchemaType } from "@google/generative-ai";
+import {
+  GenerationConfig,
+  GoogleGenerativeAI,
+  SchemaType,
+} from "@google/generative-ai";
 
 export const generateAssignment = async (API_KEY: string, prompt: string) => {
   try {
     const genAI = new GoogleGenerativeAI(API_KEY);
 
-    const generationConfig = {
+    const generationConfig: GenerationConfig = {
       temperature: 1,
       topP: 0.95,
       topK: 40,
@@ -31,6 +35,7 @@ export const generateAssignment = async (API_KEY: string, prompt: string) => {
               properties: {
                 input: { type: SchemaType.STRING },
                 expected: { type: SchemaType.STRING },
+                id: { type: SchemaType.NUMBER },
               },
               required: ["input", "expected"],
             },
@@ -61,9 +66,9 @@ export const generateAssignment = async (API_KEY: string, prompt: string) => {
           instructions:
             "Create a function `factorial` that takes an integer and returns its factorial. The factorial of n is the product of all positive integers less than or equal to n.",
           testCases: [
-            { input: "5", expected: "120" },
-            { input: "0", expected: "1" },
-            { input: "1", expected: "1" },
+            { input: "5", expected: "120", id: 1 },
+            { input: "0", expected: "1", id: 2 },
+            { input: "1", expected: "1", id: 3 },
           ],
           codeTemplate:
             "function test(){\n  //student code here\n\n};\nreturn test;",
@@ -80,7 +85,7 @@ export const generateAssignment = async (API_KEY: string, prompt: string) => {
 
     // const prePrompt = "Keep the title short";
     const prePrompt =
-      "Provide a detailed and structured coding assignment with the following: a concise title, clear description, specific requirements, examples, step-by-step instructions, test cases, a starter code template/function for student to use as a starting point, and a complete solution.";
+      "Provide a detailed and structured coding assignment with the following: a concise title, clear description, specific requirements, examples, step-by-step instructions, test cases with unique id, a starter code template/function for student to use as a starting point, and a complete solution. Which should be json parsable.";
     const result = await model.generateContent(`${prePrompt}. ${prompt}`);
 
     return result.response.text();

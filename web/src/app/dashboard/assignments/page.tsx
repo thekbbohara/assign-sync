ee9004d81108df8e317b9852a4a78febf1975642
thekbbohara <ytkbzcraft@gmail.com>
2024-12-06 23:47:06 +0545

    dry->Loader

--- web/src/app/dashboard/assignments/page.tsx
+++ web/src/app/dashboard/assignments/page.tsx
@@ -7,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Loader } from "@/components/ui/loading";
 import { cn } from "@/lib/utils";
 import { IAssignment } from "@/model/assignment";
 import { Eye, PlusCircle, Timer, Users } from "lucide-react";
@@ -74,14 +75,7 @@ export default function AssignmentsPage() {
           {/* Radial gradient for the container to give a faded look */}
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-transparent bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <p className="flex flex-col text-3xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-            {loading ? (
-              <span className="loader">
-                <span className="loader-text">loading</span>
-                <span className="load"></span>
-              </span>
-            ) : (
-              <span>No Saved Assignment</span>
-            )}
+            {loading ? <Loader /> : <span>No Saved Assignment</span>}
           </p>
         </div>
       )}
--- web/src/app/dashboard/classes/page.tsx
+++ web/src/app/dashboard/classes/page.tsx
@@ -15,6 +15,7 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { IClass } from "@/model/class";
 import { cn } from "@/lib/utils";
+import { Loader } from "@/components/ui/loading";
 
 export default function ClassesPage() {
   const { data: session } = useSession();
@@ -94,14 +95,7 @@ export default function ClassesPage() {
           {/* Radial gradient for the container to give a faded look */}
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-transparent bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <p className="flex flex-col text-3xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-            {loading ? (
-              <span className="loader">
-                <span className="loader-text">loading</span>
-                <span className="load"></span>
-              </span>
-            ) : (
-              <span>No Classes</span>
-            )}
+            {loading ? <Loader /> : <span>No Classes</span>}
           </p>
         </div>
       )}

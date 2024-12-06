45fa7e9dd960c0034f46fa57ba1934572608405b
thekbbohara <ytkbzcraft@gmail.com>
2024-12-06 23:46:22 +0545

    created loader component

---
+++ web/src/components/ui/loading.tsx
@@ -0,0 +1,8 @@
+export const Loader = () => {
+  return (
+    <span className="loader">
+      <span className="loader-text">loading</span>
+      <span className="load"></span>
+    </span>
+  );
+};

8df902206efcba5d18b7c79f75ab27c0f52dca7a
thekbbohara <ytkbzcraft@gmail.com>
2024-12-06 23:36:28 +0545

    set loading animation on student page

--- web/src/app/dashboard/students/page.tsx
+++ web/src/app/dashboard/students/page.tsx
@@ -83,7 +83,10 @@ const Students = () => {
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-transparent bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <p className="flex flex-col text-3xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
             {loading ? (
-              <>Loading....</>
+              <span className="loader">
+                <span className="loader-text">loading</span>
+                <span className="load"></span>
+              </span>
             ) : (
               <>
                 <span>No students.</span>

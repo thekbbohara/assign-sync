fb36723fa4a7a11bb8e301f6f3d447627d39970d
thekbbohara <ytkbzcraft@gmail.com>
2024-12-06 22:28:08 +0545

    no student bg

--- web/src/app/dashboard/students/page.tsx
+++ web/src/app/dashboard/students/page.tsx
@@ -47,19 +47,18 @@ const Students = () => {
 
   return (
     <>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Email</TableHead>
-            <TableHead>Joined</TableHead>
-            <TableHead>Progress</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {students &&
-            students.length >= 1 &&
-            students.map((student) => (
+      {students && students.length >= 1 ? (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Joined</TableHead>
+              <TableHead>Progress</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {students.map((student) => (
               <TableRow key={String(student?._id)}>
                 <TableCell>{student?.name}</TableCell>
                 <TableCell>{student?.email}</TableCell>
@@ -74,8 +73,17 @@ const Students = () => {
                 </TableCell>
               </TableRow>
             ))}
-        </TableBody>
-      </Table>
+          </TableBody>
+        </Table>
+      ) : (
+        <div className="h-[calc(100svh-70px)] w-full dark:bg-transparent bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-transparent bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+          <p className="text-3xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+            No students.
+          </p>
+        </div>
+      )}
     </>
   );
 };

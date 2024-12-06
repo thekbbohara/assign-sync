58442b895defa229432a9fa45d07f12c2b9e73e3
thekbbohara <ytkbzcraft@gmail.com>
2024-12-06 23:29:13 +0545

    loading animation on class

--- web/src/app/dashboard/classes/page.tsx
+++ web/src/app/dashboard/classes/page.tsx
@@ -21,6 +21,7 @@ export default function ClassesPage() {
   const userId = session?.user?.id;
   const [classes, setClasses] = useState<IClass[]>([]);
   const [refresh, setRefresh] = useState(true);
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
     const getClasses = async () => {
       const res = await fetch("/api/class", {
@@ -92,7 +93,14 @@ export default function ClassesPage() {
           {/* Radial gradient for the container to give a faded look */}
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-transparent bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <p className="flex flex-col text-3xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-            <span>No Classes.</span>
+            {loading ? (
+              <span className="loader">
+                <span className="loader-text">loading</span>
+                <span className="load"></span>
+              </span>
+            ) : (
+              <span>No Classes</span>
+            )}
           </p>
         </div>
       )}

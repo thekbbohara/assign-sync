59214c2c1e7faa68975a4cb8cacac64d89ddea0a
thekbbohara <ytkbzcraft@gmail.com>
2024-12-06 23:08:48 +0545

    add assignment btn on page

--- web/src/app/dashboard/assignments/page.tsx
+++ web/src/app/dashboard/assignments/page.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { IAssignment } from "@/model/assignment";
-import { Eye, Timer, Users } from "lucide-react";
+import { Eye, PlusCircle, Timer, Users } from "lucide-react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -71,8 +71,17 @@ export default function AssignmentsPage() {
         <div className="h-[calc(100svh-70px)] w-full dark:bg-transparent bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
           {/* Radial gradient for the container to give a faded look */}
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-transparent bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-          <p className="text-3xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-            No Saved Assignment
+          <p className="flex flex-col text-3xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+            <span>No Saved Assignment</span>
+            <Button
+              className="bg-none hover:bg-transparent text-3xl flex gap-2 group"
+              variant="ghost"
+            >
+              <PlusCircle className="text-white/[0.4] text-3xl group-hover:text-white" />
+              <Link href={"/dashboard/assignments/new"} className="text-2xl  ">
+                New Assignment
+              </Link>
+            </Button>
           </p>
         </div>
       )}
